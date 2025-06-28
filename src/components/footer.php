7931861fdb65f002e904b7c9a8b45283700fbb06
guirque <guirque@gmail.com>
2025-06-28 20:28:00 -0300

    feat: adding common page components

---
+++ src/components/footer.php
@@ -0,0 +1,5 @@
+<footer class="container-fluid text-bg-dark py-2 mt-3">
+    <div class="row">
+        <div class="col">All rights reserved.</div>
+    </div>
+</footer>
---
+++ src/components/navbar.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand navbar-dark bg-dark mb-3">
+    <div class="container">
+
+        <!-- Icon -->
+        <img class="navbar-brand" src="./img/icon.png" style="width: 40px;"></img>
+
+        <!-- Links -->
+        <ul class="navbar-nav">
+            <li class="nav-item"><a href="" class="nav-link">Public Gallery</a></li>
+            <li class="nav-item"><a href="" class="nav-link">Your Gallery</a></li>
+            <li class="nav-item"><a href="" class="nav-link">Login</a></li>
+        </ul>
+    </div>
+</nav>
