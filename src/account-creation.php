e7b58a5800b1fd3f2ed880010170829c7309a48d
guirque <guirque@gmail.com>
2025-06-28 21:25:27 -0300

    feat: adding account creation

---
+++ src/account-creation.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+<body>
+
+    <?php require_once "components/navbar.php" ?>
+
+    <div class="container d-flex gap-4 pb-5 justify-content-center" style="flex-direction: column; min-height: 100vh;">
+    <div class="container">
+
+        <div class="card bg-light-subtle">
+            <div class="card-header">
+                <h1>Create Account</h1>
+            </div>
+
+            <div class="card-body">
+                <div class="row">
+
+                    <form action="./db_actions/create_user.php" method="post" enctype="multipart/form-data" class="vstack g-2">
+                        
+                        <label for="username" class="form-label">Username: </label>
+                        <input type="text" name="username" id="username" class="form-control">
+                
+                        <label for="password" class="form-label">Password: </label>
+                        <input type="password" name="password" id="password" class="form-control">
+                        
+                        <label for="email" class="form-label">Email: </label>
+                        <input type="email" name="email" id="email" class="form-control">
+                
+                        <label for="profile_picture" class="form-label">Profile Picture: </label>
+                        <input type="file" name="profile_picture" id="profile_picture" class="form-control">
+                        
+                        <br/>
+                        <input type="submit" value="Submit" class="btn btn-success">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+        <?php require_once "components/footer.php" ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ src/db_actions/create_user.php
@@ -0,0 +1,59 @@
+<?php
+
+    # IMPORTANT REFERENCES
+    # https://stackoverflow.com/questions/8922056/what-is-the-best-way-to-upload-and-store-pictures-on-the-site
+    # https://stackoverflow.com/questions/8103860/move-uploaded-file-gives-failed-to-open-stream-permission-denied-error
+    # https://www.w3schools.com/php/php_file_upload.asp
+
+    # We need to give permission to move the file on /tmp/
+    try
+    {
+        require_once "connection.php";
+        
+        $db_manager_obj = new DBManager();
+
+        echo "<br>";
+
+        $file_path = "/var/www/webdev.com/".getenv("USER_PROFILE_PATH");
+
+        # Uploading Photo --------------------------------------------------------
+        # If temporary file exists
+        if(
+            isset($_FILES['profile_picture']) && file_exists($_FILES['profile_picture']['tmp_name'])
+            && $_FILES['profile_picture']['size'] < getenv("MAX_IMAGE_SIZE") # If file is of acceptable size
+        )
+        {
+            $split_type = explode('/', $_FILES['profile_picture']['type']);
+            $file_extension = ".".$split_type[count($split_type)-1];
+            $ACCEPTED_FILE_TYPES = array('.png', '.jpg', '.jpeg');
+            
+            # Checking file extension
+            if(in_array($file_extension, $ACCEPTED_FILE_TYPES))
+            {
+                # Analyzing file
+                
+                $file_name = uniqid('', true).$file_extension;
+                # Uploading file to photos folder
+                if(!move_uploaded_file($_FILES['profile_picture']['tmp_name'], $file_path.$file_name))
+                {
+                    echo "<h1>An error occurred when trying to save image.</h1>";
+                }
+    
+                # Saving user on database
+                $encrypted_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                $db_manager_obj->create_user($_POST['username'], $encrypted_password, $_POST['email'], $file_name);
+                echo "<h1>User creation operation finished.</h1>";
+                # echo "<hr><img src=\"http://webdev.com/photos/user_profile/$file_name\" alt=\"user photo\" width=\"400\"/><hr>";
+            }
+            else echo "<h1>File extension $file_extension not accepted.</h1>";
+
+        }
+        else echo "<h1>An error occurred when trying to upload profile picture.</h1>";
+
+    }
+    catch(Exception $e)
+    {
+        echo "An error occurred: ".$e->getMessage();
+    }
+
+?>
